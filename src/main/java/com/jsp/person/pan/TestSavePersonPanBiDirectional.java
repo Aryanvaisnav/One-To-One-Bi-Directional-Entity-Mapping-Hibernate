3e39ca68b1d71f52ea33f7eee9d5f21b0af5eea5
Aryan Vaisnav <aryanvaisnav18@gmail.com>
2023-10-13 22:56:26 +0530

    commit

---
+++ src/main/java/com/jsp/person/pan/TestSavePersonPanBiDirectional.java
@@ -0,0 +1,37 @@
+package com.jsp.person.pan;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class TestSavePersonPanBiDirectional {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("aryan");
+		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		
+		Person person = new Person();
+		person.setName("PQR");
+		person.setEmail("pqr@gmail.com");
+		person.setCno(3683085282637l);
+		
+		Pan pan = new Pan();
+		pan.setAddress("Banglore");
+		
+		pan.setPerson(person);
+		person.setPan(pan);
+		
+		entityTransaction.begin();
+		entityManager.persist(person);
+		entityManager.persist(pan);
+		entityTransaction.commit();
+		
+
+	}
+
+}
